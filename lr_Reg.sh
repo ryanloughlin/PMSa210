f5d3456990044936a195bf2e01259e67705438b6
ryanloughlin <rloughlin@mac.com>
2016-07-05 15:00:43 -0400

    Update lr_Reg.sh

--- lr_Reg.sh
+++ lr_Reg.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+#################################################################################
+#
+#   Registration for Adobe Lightroom 5
+#   Place in /usr/local/outset/login-once
+#
+#################################################################################
+
 defaults write com.adobe.Lightroom5.plist firstLaunchHasRun30 -bool YES
 defaults write com.adobe.Lightroom5.plist RegistrationField_CountryRegion -int 1
 exit 0
